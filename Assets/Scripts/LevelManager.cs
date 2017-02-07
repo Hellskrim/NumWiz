cb7e09620c624849d1eeb01767561765ab0b7780
Hellskrim <sierszecki.m@gmail.com>
2017-02-07 18:59:26 +0100

    Number Wizard
    Unity simple game made for android

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requested "+name);
+        SceneManager.LoadScene(name);
+    }
+
+}
---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class NumberWizard : MonoBehaviour {
+
+    // Use this for initialization
+    int max;
+    int min;
+    int guess;
+
+    public Text numberGuess;
+
+    int maxGuess = 7;
+
+    void Start () {
+        max = 1000;
+        min = 1;
+        nextGuess();
+    }
+
+    public void getHeigher()
+    {
+        min = guess;
+        nextGuess();
+    }
+    public void getLower()
+    {
+        max = guess;
+        nextGuess();
+    }
+    void nextGuess()
+    {
+        guess = Random.Range(min,max+1);
+        numberGuess.text = "Is that "+guess.ToString()+"?";
+        maxGuess--;
+        if (maxGuess <= 0) { SceneManager.LoadScene("Win"); }
+    }
+    public void isEqual()
+    {
+        SceneManager.LoadScene("Lose");
+    }
+}
